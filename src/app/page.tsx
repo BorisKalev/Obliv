804afc2241f274d4208f0a8225477c6ebf6f60f5
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-10-01 17:36:35 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import { Header } from "@/sections/Header";
+<<<<<<< HEAD
+import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
+import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <LogoTicker />
+      <ProductShowcase />
+      <Pricing />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
+    </>
+  );
+=======
+export default function Home() {
+  return <Header />;
+>>>>>>> temp-branch
+}
